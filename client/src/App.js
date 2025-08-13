8e2aa0c6d7bf2bc315276df7c7da407ef78d108f
Jean-AT <jeanpoolariass2020@hotmail.com>
2025-08-12 22:35:53 -0500

    Add the new form to registration in pages

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,21 @@ import {BrowserRouter as Router, Route, Link, Routes} from "react-router-dom"
 import './App.css';
 
 import Login from './pages/Login';
+import Registration from './pages/Registration'
 
 function App() {
   return (
     <div className="App">
         <Router>
           <div className='navbar'>
-          <Link to="/Login"> Login</Link>
-          <Link to="/"> Home</Link>
+            <Link to="/Login"> Login</Link>
+            <Link to="/Registration"> Registration</Link>
+            <Link to="/"> Home</Link>
           </div>
         <Routes>
           <Route path="/Login" element={<Login/>} />
           <Route path="/"/>
+          <Route path="/Registration" element={<Registration/>} />
         </Routes>
       </Router>
     </div>
--- client/src/pages/Registration.js
+++ client/src/pages/Registration.js
@@ -1,1 +1,81 @@
-import React from "react";
+import {useForm} from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup"; // Integración Yup
+import axios from "axios";//For the request to que api
+import {useNavigate} from "react-router-dom";//Navigate in the pages
+import * as Yup from "yup";//Require the password and the username in a default long
+
+export default function Login(){
+    const navigate = useNavigate();
+    const validationSchema=Yup.object().shape({
+        username:Yup.string().min(3).max(15).required(),
+        password:Yup.string().min(5).max(20).required(),
+        email:Yup.string().min(5).max(40),
+        bornDate:Yup.date(),
+        firstName:Yup.string().min(3).max(10),
+        lastName:Yup.string()
+    });
+    const OnSubmit= async(data)=>{
+        try{
+            await axios.post("http://localhost:3000/auth/registration",data).then(()=>{
+                console.log(data);
+                navigate("/");
+            });
+        }catch (err){
+            console.log(err);
+        }
+    };
+    const {register,handleSubmit, formState:{errors}}=useForm({
+        defaultValues:{
+            username:"",
+            password:"",
+            email:"",
+            bornDate:null,
+            firstName:"",
+            lastName:"",
+        },
+        resolver:yupResolver(validationSchema)
+    });
+    
+
+    return(
+        <dev>
+            <form onSubmit={handleSubmit(OnSubmit)}>
+                <input 
+                {...register("username")}
+                placeholder="Username"
+                autoComplete="off"
+                />
+                {errors.username && <p>{errors.username.message}</p>}
+                <input
+                {...register("password")}
+                placeholder="Password"
+                type="password"
+                autoComplete="off"
+                />
+                {errors.username && <p>{errors.username.message}</p>}
+                <input
+                {...register("email")}
+                placeholder="Email"
+                autoComplete="off"
+                />
+                <input
+                {...register("bornDate")}
+                placeholder="Born Date"
+                type="date"
+                autoComplete="off"
+                />
+                <input
+                {...register("firstName")}
+                placeholder="First Name"
+                autoComplete="off"
+                />
+                <input
+                {...register("lastName")}
+                placeholder="Last Name"
+                autoComplete="off"
+                />
+                <button type="submit">Registration</button>
+            </form>
+        </dev>
+    );
+}
